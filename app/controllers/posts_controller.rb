ce82b6411b05e3d33b533e6a8072212a18cdbfda
Kip Kosek <kipkosek@gmail.com>
2016-12-06 10:54:47 -0500

    Assignment 18 Introduction to CRUD: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.censored
+
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  def self.censored
+    self.all.each { |p| p.title = "SPAM" if p.id == 1 || p.id % 5 == 0 }
+  end
 end
