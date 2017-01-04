c3e38a6d881edfcb1502f8883d19bc539279a755
Kip Kosek <kipkosek@gmail.com>
2017-01-04 14:49:39 -0500

    Assignment 30 Voting: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
+  after_create :create_vote
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   default_scope { order('rank DESC') }
@@ -25,5 +26,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+private
+  def create_vote
+    user.votes.create(post: self, value: 1)
   end
 end
