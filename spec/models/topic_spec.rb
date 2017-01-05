7a83ec3d59fc93c3717a2a5d519f33bd1f42d16d
Kip Kosek <kipkosek@gmail.com>
2017-01-05 13:54:35 -0500

    Checkpoint 32 Public Profiles: Completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Topic, type: :model do
 
   describe "attributes" do
     it "has name, description, and public attributes" do
-      expect(topic).to have_attributes(name: name, description: description, public: public)
+      expect(topic).to have_attributes(name: topic.name, description: topic.description, public: public)
     end
     it "is public by default" do
       expect(topic.public).to be(true)
